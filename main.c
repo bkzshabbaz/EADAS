1f6a8b0afaefe82e11fce588e29bb1cabd885385
Sammy Lin <sammy.lin@alum.rit.edu>
2015-10-18 00:07:46 -0400

    Got interrupts working with the LSM9DS0.

--- main.c
+++ main.c
@@ -1,109 +1,94 @@
 #include <msp430.h> 
 
-#define SCK 	BIT4    // Serial clock P1.4 SCL
-#define CSG		BIT2	//CSG (slave select) P3.2
-#define SDOG	BIT7	//SDOG on pin MISO P1.7
-#define SDA		BIT6	//SDA   MOSI (SDA) P1.6
 #define LED0	BIT0	//LED 0
 #define LED1	BIT7	//LED 1
 
 volatile unsigned char RXData = 0;
-volatile unsigned char TXData;
+volatile unsigned char TXData = 0;
+volatile unsigned int sent_data = 0;
+
 void initialize_clock()
 {
+	P9OUT |= BIT7;
+	P9DIR |= BIT7;
+
+	P1DIR |= BIT0 | BIT4; 		//Configure LED and Chip Select for Gyro
+	P3DIR |= BIT2;				//Configure Chip Select for XM
+
+	P1SEL1 |= BIT5;                    // Configure sclk
+	P1SEL1 &= ~ BIT4;
+	P3SEL1 &= ~BIT2;
+
+	P2SEL0 |= BIT0 | BIT1;                    // Configure SOMI and MISO
+	PJSEL0 |= BIT4 | BIT5;                    // For XT1
+
 	// Disable the GPIO power-on default high-impedance mode to activate
 	// previously configured port settings
 	PM5CTL0 &= ~LOCKLPM5;
 
-	// Startup clock system with max DCO setting ~8MHz
-	CSCTL0_H = CSKEY >> 8;                    // Unlock clock registers
-	CSCTL1 = DCOFSEL_3 | DCORSEL;
-	CSCTL2 = SELA__VLOCLK | SELS__DCOCLK | SELM__DCOCLK;
+	// XT1 Setup
+	CSCTL0_H = CSKEY >> 8;                    // Unlock CS registers
+	CSCTL1 = DCOFSEL_0;                       // Set DCO to 1MHz
+	CSCTL1 &= ~DCORSEL;
+	CSCTL2 = SELA__LFXTCLK | SELS__DCOCLK | SELM__DCOCLK;
 	CSCTL3 = DIVA__1 | DIVS__1 | DIVM__1;     // Set all dividers
+	CSCTL4 &= ~LFXTOFF;
+	do
+	{
+		CSCTL5 &= ~LFXTOFFG;                    // Clear XT1 fault flag
+		SFRIFG1 &= ~OFIFG;
+	}while (SFRIFG1&OFIFG);                   // Test oscillator fault flag
 	CSCTL0_H = 0;                             // Lock CS registers
-
-}
-
-void initialize_uart()
-{
-	// Configure GPIO
-	P2SEL0 |= BIT0 | BIT1;                    // USCI_A0 UART operation
-	P2SEL1 &= ~(BIT0 | BIT1);
-
-
-	// Configure USCI_A0 for UART mode
-	UCA0CTLW0 = UCSWRST;                      // Put eUSCI in reset
-	UCA0CTLW0 |= UCSSEL__SMCLK;               // CLK = SMCLK
-	UCA0BR0 = 52; //9600
-	UCA0BR1 = 0x00;
-	UCA0MCTLW |= UCOS16 | UCBRF_1 | 0x4900;
-	UCA0CTLW0 &= ~UCSWRST;                    // Initialize eUSCI
 }
 
 void initialize_spi()
 {
-	//Start SPI configuration here
-	UCB0CTL1 |= UCSWRST;                      // **Put state machine in reset**
-	UCB0CTL0 |= UCMST|UCSYNC|UCCKPL|UCMSB;    // 3-pin, 8-bit SPI master, Clock polarity high, MSB
-	UCB0CTL1 |= UCSSEL_2;
-	UCA0CTLW0 |= UCSSEL__ACLK;
 
-	UCB0BR0 = 0x02;                           // /2
-	UCB0BR1 = 0;                              //
-	//UCB1MCTL = 0;                             // No modulation
-
-	/* Configure the ports */
-	P3OUT &= ~CSG;	//Chip select P3.2
-
-	//P1SEL1 = 0; //Clear first
-	P1SEL0 |=  (SDOG | SDA | SCK);  		//Initialize P1.4/6/7 for clock MOSI/MISO
-	P1SEL1 &= ~(SDOG | SDA | SCK);
-
-	P1DIR |= SCK;
-	P1DIR &= ~SDOG;
-	P3DIR |= CSG;
-	/* End configuring the ports */
-	volatile unsigned int i;
-	i = 1000;                          // SW Delay to let everything settle
-	do i--;
-	while(i != 0);
-	UCB0CTL1 &= ~UCSWRST;        			// **Initialize USCI state machine**
-
-	/*enable interrupts*/
-	UCB0IE |= (UCRXIE | UCTXIE);
-
-	P3OUT |= CSG;							//reset slave
-	P3OUT &= ~CSG;
+	// Configure USCI_A0 for SPI operation
+	UCA0CTLW0 = UCSWRST;                      // **Put state machine in reset**
+										// 4-pin, 8-bit SPI master
+	UCA0CTLW0 |= UCMST | UCSYNC | UCCKPL | UCMSB | UCMODE_1 | UCSTEM;
+										// Clock polarity high, MSB
+	UCA0CTLW0 |= UCSSEL__ACLK;                // ACLK
+	UCA0BR0 = 0x02;                           // /2
+	UCA0BR1 = 0;                              //
+	UCA0MCTLW = 0;                            // No modulation
+	UCA0CTLW0 &= ~UCSWRST;                    // **Initialize USCI state machine**
+	UCA0IE |= UCRXIE | UCTXIE;                         // Enable USCI_A0 RX interrupt
 }
 
-void print_uart(unsigned char *character) {
-	while (*character != '\0') {
-		while (!(UCA0IFG & UCTXIFG));
-    	UCA0TXBUF = *character++;
-	}
-}
-
-#pragma vector=USCI_B0_VECTOR
-__interrupt void USCI_B0_ISR(void)
+#if defined(__TI_COMPILER_VERSION__) || defined(__IAR_SYSTEMS_ICC__)
+#pragma vector=USCI_A0_VECTOR
+__interrupt void USCI_A0_ISR(void)
+#elif defined(__GNUC__)
+void __attribute__ ((interrupt(USCI_A0_VECTOR))) USCI_A0_ISR (void)
+#else
+#error Compiler not supported!
+#endif
 {
-	volatile unsigned int i;
+  unsigned char data_in = 0;
+  switch(__even_in_range(UCA0IV, USCI_SPI_UCTXIFG))
+  {
+    case USCI_NONE: break;
+    case USCI_SPI_UCRXIFG:
+      RXData = UCA0RXBUF;
+      UCA0IFG &= ~UCRXIFG;
+      if (RXData == 0xD4)
+    	  P9OUT |= LED1;
 
-
-	switch(__even_in_range(UCB0IV, USCI_SPI_UCTXIFG))
-	  {
-	    case USCI_NONE: break;
-	    case USCI_SPI_UCRXIFG:
-			RXData = UCB0RXBUF;
-			UCB0IFG &= ~UCRXIFG;
-			P1OUT &= ~LED0;
-	      	break;
-	    case USCI_SPI_UCTXIFG:
-			UCA0TXBUF = TXData;                   // Transmit characters
+      break;
+    case USCI_SPI_UCTXIFG:
+		P1OUT &= ~BIT4;
+		P3OUT |= BIT2;
+		if (TXData == 0x00)
 			UCA0IE &= ~UCTXIE;
-			P9OUT &= ~LED1;
-			// __bic_SR_register_on_exit(LPM0_bits); // Wake up to setup next TX
-	      	break;
-	  }
+		UCA0TXBUF = TXData;                   // Transmit characters
+		TXData = 0x00;
+
+		__delay_cycles(1000);
+      break;
+    default: break;
+  }
 }
 
 /*
@@ -114,7 +99,7 @@ int main(void) {
 
 	//
     initialize_clock();
-    initialize_uart();
+    initialize_spi();
     volatile unsigned int i;
 
     P1OUT |= LED0;
@@ -122,16 +107,13 @@ int main(void) {
 
 	P9OUT |= LED1;
 	P9DIR |= LED1;
-	//initialize_spi();
 
-	print_uart("Hello world\n\r");
-	//__bis_SR_register(GIE);     // enable interrupts
-	TXData = 0x01;
-	UCB0TXBUF = TXData;
+	__bis_SR_register(GIE);     // enable interrupts
+	TXData = (0x80 | (0x0F & 0x3F));
 	for(;;) {
-		P1OUT ^= BIT0;				// Toggle P1.0 using exclusive-OR
+		P1OUT ^= LED0;				// Toggle P1.0 using exclusive-OR
 
-		i = 10000;                          // SW Delay
+		i = 100000;                          // SW Delay
 		do i--;
 		while(i != 0);
 	}
