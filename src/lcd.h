c35c18c17fbad950aa68fccb308792ba8e76a7fe
thembeddedguy <npatel33@gmu.edu>
2015-11-17 21:03:55 -0500

    lcd symols added

--- src/lcd.h
+++ src/lcd.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #ifndef _LCD_H
 #define _LCD_H
 
@@ -21,22 +20,3 @@ void lcdPrint(char *c, uint8_t start, uint8_t end);
 void lcdBlinkSym(uint8_t symbol);
 
 #endif
-=======
-#ifndef _LCD_H
-#define _LCD_H
-
-#include<stdint.h>
-#define ALPHA_NUM 0
-#define SYMBOL 1
-
-void lcdInit(void);
-void lcdOff(void);
-void lcdOn(void);
-void lcdSegOn(void);
-void lcdSegOff(void);
-void lcdIOInit(void);
-void lcdPrint(char *c, int alphaNum,uint16_t start,uint16_t end);
-void lcdTimerInit(void);
-
-#endif /* _LCD_H*/
->>>>>>> ed6b7b40637a6cd3743fb2b372f3c51ec6154a49
